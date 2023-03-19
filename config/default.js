9da34d4055490362dafac0dbbca4973196453d1d
Anjorin olutosin <anjorintosin080@gmail.com>
2023-03-19 14:15:40 +0100

    commit

---
+++ config/default.js
@@ -0,0 +1,20 @@
+module.exports = {
+  swagger: {
+    active: true,
+    host: "api.dev.patpay.com",
+  },
+  environment: process.env.ENVIRONMENT,
+  server: {
+    connection: {
+      compression: false,
+      port: process.env.PORT,
+      routes: {
+        cors: true,
+      },
+    },
+  },
+  mongodb: {
+    url: process.env.DB_HOST,
+  },
+  cors: { origin: ['*'] },
+};
